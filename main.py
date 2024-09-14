b102e1e09ab9ce90f4a41feb07850f3d2d9f4387
Angela Wang <wangela472@gmail.com>
2024-09-14 10:29:12 -0400

    rm unused code

--- main.py
+++ main.py
@@ -321,6 +321,3 @@ camera_thread.start()
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 # To run the server, use: uvicorn main:app --reload
-
-# import uvicorn
-# uvicorn.run(app, host="0.0.0.0", port=8080)
